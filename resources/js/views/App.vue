bd153bf48f49f5b004ad43283aa5f5080d318b1f
Alex12012000 <arcex.29@gmail.com>
2022-09-02 17:14:44 +0200

    Finito

---
+++ resources/js/views/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <a href="/admin">Area Privata</a>
+        <h1>Ciao sono guest Homepage </h1>
+    </div>
+</template>
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Sono la dashboard privata</h1>
+@endsection
