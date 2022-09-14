92320b4fdc741fe86399365f55f3fb34529129dc
Alex12012000 <arcex.29@gmail.com>
2022-09-14 15:29:30 +0200

    Single page + api

---
+++ resources/js/pages/SinglePost.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div v-if="post">
+            <h1>{{post.title}}</h1>
+            <div>
+                <p>{{post.content}}</p>
+            </div>
+            <!-- Category -->
+            <div class="category">
+                
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SinglePost',
+    data() {
+        return {
+            post: null,
+        }
+    },
+    methods: {
+        getPost() {
+            axios.get(`/api/posts/${this.$route.params.slug}`)
+            .then((response) => {
+                this.post = response.data.post;
+                console.log(response.data.post)
+            })
+        }
+    },
+    mounted() {
+        this.getPost()
+    },
+}
+</script>
+
+<style>
+
+</style>
